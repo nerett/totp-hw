7b7ecf4a2e840bb1d8fa04d1f5043a295e1a8361
nerett <pt-mvb@inbox.lv>
2024-12-14 19:46:20 +0300

    added ui & totp timers, button handlers, lcd, refactored host API codes

--- target/target.ino
+++ target/target.ino
@@ -1,11 +1,22 @@
 #include <TOTP.h>
-
 #include <Base32.h>
+#include <LiquidCrystal.h>
+
+const int rs = PB11, en = PB10, d4 = PB0, d5 = PB1, d6 = PC13, d7 = PC14;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 const int SERIAL_BAUD_RATE = 9600;
 
-const char GET_LIST_OF_SITES_ID = '0';
-const char GET_SITE_AUTH_CODE = '1';
+enum class HostAPICode = {
+  GET_SITES = '0',
+  GET_AUTH_CODE = '1',
+  ADD_SITE = '2',
+  REM_SITE = '3',
+  SET_TIME = '4'
+};
+
+// const char GET_LIST_OF_SITES_ID = '0';
+// const char GET_SITE_AUTH_CODE = '1';
 
 const int TIME_STEP = 30;
 
@@ -14,18 +25,41 @@ const char* sites_id[SIZE]   = {"1", "2"};
 const char* sites_hash[SIZE] = {"hash1", "hash2"};
 const char* sites_encoded_code[SIZE] = {"code1", "2S6NHETOPYDV3K5V"};
 
+const int ui_timer_period = 5000000000;
+const int totp_timer_period = 1000000000;
+
+volatile long global_current_time = 0;
+
+volatile bool button_pressed = false;
+volatile bool ui_timer_updated = false;
+
 void setup() {
   Serial.begin(SERIAL_BAUD_RATE);
+
+  pinMode(PA0, INPUT);
+  attachInterrupt(PA0, on_button_push, FALLING);
+
+  Timer3.pause();
+  Timer3.setPeriod(ui_timer_period);
+  Timer3.attachInterrupt(TIMER_UPDATE_INTERRUPT, on_ui_timer_update);
+
+  Timer4.pause();
+  Timer4.setPeriod(totp_timer_period);
+  Timer4.attachInterrupt(TIMER_UPDATE_INTERRUPT, on_totp_timer_update);
 }
 
 void loop() {
   if (Serial.available() > 0) {
     char incoming_byte = Serial.read();
 
-    if (incoming_byte == GET_LIST_OF_SITES_ID) {
+    if (incoming_byte == HostAPICode::GET_SITES) {
       get_list_of_sites_id();
-    } else if (incoming_byte == GET_SITE_AUTH_CODE) {
+    } else if (incoming_byte == HostAPICode::GET_AUTH_CODE) {
       get_site_auth_code();
+    } else if (incoming_byte == HostAPICode::SET_TIME) {
+      set_time();
+    } else {
+      Serial.println("Error: HostAPICode not implemented!");
     }
   }
 }
@@ -105,4 +139,28 @@ String get_site_auth_code(const char* site_encoded_code, long current_time) {
   String site_auth_code = totp.getCodeFromSteps(time_steps);
 
   return site_auth_code;
+}
+
+void on_button_push() {
+  button_pressed = true;
+}
+
+void on_ui_timer_update() {
+  ui_timer_updated = true;
+}
+
+void on_totp_timer_update() {
+  global_current_time++;
+}
+
+set_time() {
+  char *endptr;
+  long curr_time = strtol(get_string().c_str(), &endptr, 10);
+
+  Timer4.pause();
+  Timer4.refresh();
+
+  global_current_time = curr_time;
+  
+  Timer3.resume();
 }
